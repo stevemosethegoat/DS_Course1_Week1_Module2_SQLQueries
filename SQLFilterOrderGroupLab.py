9c08088624c26ca809a376da5f2f6238b59b05a0
stevemosethegoat <smosethegoat@gmail.com>
2026-04-25 15:59:58 +0300

    added the main.py

--- SQLFilterOrderGroupLab.py
+++ SQLFilterOrderGroupLab.py
@@ -25,7 +25,7 @@
 # 
 # Table Name: planets
 
-# In[118]:
+# In[152]:
 
 
 # CodeGrade step0
@@ -46,7 +46,7 @@ pd.read_sql("""SELECT * FROM planets; """, conn1)
 # ### Step 1
 # Return all the columns for planets that have 0 moons.
 
-# In[119]:
+# In[153]:
 
 
 # CodeGrade step1
@@ -60,7 +60,7 @@ df_no_moons
 # ### Step 2
 # Return the name and mass of each planet that has a name with exactly 7 letters. Avoid hard coding this filter subset as much as possible.
 
-# In[120]:
+# In[154]:
 
 
 # CodeGrade step2
@@ -77,7 +77,7 @@ df_name_seven
 # 
 # Return the name and mass for each planet that has a mass that is less than or equal to 1.00.
 
-# In[121]:
+# In[155]:
 
 
 # CodeGrade step3
@@ -91,7 +91,7 @@ df_mass
 # 
 # Return all the columns for planets that have at least one moon and a mass less than 1.00.
 
-# In[122]:
+# In[156]:
 
 
 # CodeGrade step4
@@ -106,7 +106,7 @@ df_mass_moon
 # 
 # Return the name and color of planets that have a color containing the string "blue".
 
-# In[123]:
+# In[157]:
 
 
 # CodeGrade step5
@@ -125,7 +125,7 @@ df_blue
 # 
 # Table Name: dogs
 
-# In[124]:
+# In[158]:
 
 
 # CodeGrade step0
@@ -143,7 +143,7 @@ pd.read_sql("SELECT * FROM dogs;", conn2)
 # ### Step 6
 # Return the name, age, and breed of all dogs that are hungry (binary flag of 1) and sort them from youngest to oldest.
 
-# In[125]:
+# In[159]:
 
 
 # CodeGrade step6
@@ -158,7 +158,7 @@ df_hungry
 # ### Step 7
 # Return the name, age, and hungry columns for hungry dogs between the ages of two and seven. This query should also sort these dogs in alphabetical order.
 
-# In[126]:
+# In[160]:
 
 
 # CodeGrade step7
@@ -176,20 +176,16 @@ df_hungry_ages
 # 
 # Return the name, age, and breed for the 4 oldest dogs. Sort the result alphabetically based on the breed.
 
-# In[127]:
+# In[161]:
 
 
 # CodeGrade step8
 # Replace None with your code
 df_4_oldest = pd.read_sql("""
     SELECT name, age, breed
-    FROM (
-        SELECT name, age, breed
-        FROM dogs
-        ORDER BY age DESC
-        LIMIT 4
-    )
-    ORDER BY breed ASC;
+    FROM dogs
+    ORDER BY age DESC
+    LIMIT 4;
 """, conn2)
 df_4_oldest
 
@@ -200,7 +196,7 @@ df_4_oldest
 # 
 # Table Name: babe_ruth_stats
 
-# In[128]:
+# In[162]:
 
 
 # CodeGrade step0
@@ -220,7 +216,7 @@ SELECT * FROM babe_ruth_stats; """, conn3)
 # 
 # Return the total number of years that Babe Ruth played professional baseball
 
-# In[129]:
+# In[163]:
 
 
 # CodeGrade step9
@@ -234,7 +230,7 @@ df_ruth_years
 # 
 # Return the total number of homeruns hit by Babe Ruth during his career.
 
-# In[130]:
+# In[164]:
 
 
 # CodeGrade step10
@@ -250,7 +246,7 @@ df_hr_total
 # 
 # For each team that Babe Ruth has played on, return the team name and the number of years he played on that team, aliased as 'number_years'.
 
-# In[131]:
+# In[165]:
 
 
 # CodeGrade step11
@@ -263,7 +259,7 @@ df_teams_years
 # 
 # For each team that Babe Ruth played on and averged over 200 at bats with, return the team name and average number of at bats, aliased as 'average_at_bats'.
 
-# In[132]:
+# In[166]:
 
 
 # CodeGrade step12
@@ -280,7 +276,7 @@ df_at_bats
 
 # #### Close the connections
 
-# In[133]:
+# In[167]:
 
 
 # Run this cell without changes
